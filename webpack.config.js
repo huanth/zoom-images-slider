2cadf536ae91880f74ba7fd0fdffc8c61200244b
HuanTH <huan.huy.truong@gmail.com>
2023-06-16 10:29:36 +0700

    Add dep in package

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    // Other webpack configuration options...
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env'],
+                },
+            },
+        }, ],
+    },
+};
